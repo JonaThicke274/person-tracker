fea404ab1d1f3f3a60c4e6c18d2c5ef52a425c20
ldholl <lacy.d.holleman@gmail.com>
2022-11-09 14:40:47 -0500

    made the log in and log out links even

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,28 +2,28 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const {Person, Notes } = require('../models');
 
-// router.get('/', (req, res) => {
-//     Person.findAll({
-//         attributes: [
-//             'id',
-//             'last_name',
-//             'first_name'
-//         ]
-//     })
-//     .then(dbPersonData => {
-//         console.log(dbPersonData);
-//         const persons = dbPersonData.map(post=> post.get({ plain: true}))
-//         res.render('homepage', { 
-//             persons,
-//             loggedIn: req.session.loggedIn
-//          }) 
+router.get('/', (req, res) => {
+    Person.findAll({
+        attributes: [
+            'id',
+            'last_name',
+            'first_name'
+        ]
+    })
+    .then(dbPersonData => {
+        console.log(dbPersonData);
+        const persons = dbPersonData.map(post=> post.get({ plain: true}))
+        res.render('homepage', { 
+            persons,
+            loggedIn: req.session.loggedIn
+         }) 
         
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err)
-//     })
-// });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+});
 
 router.get(`/login`, (req, res) => {
     if (req.session.loggedIn) {
