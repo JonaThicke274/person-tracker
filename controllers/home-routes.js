90ff1db88ec7f9da386514f09611ea821186c684
ldholl <lacy.d.holleman@gmail.com>
2022-11-09 14:20:04 -0500

    altered login form and created signup modal

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,23 +1,29 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const {Person} = require('../models');
+const {Person, Notes } = require('../models');
 
-router.get('/', (req, res) => {
-    Person.findAll({
-        attributes: [
-            'id',
-            'last_name',
-            'first_name'
-        ]
-    })
-    .then(dbPersonData => {
-        res.render('homepage', dbPersonData[0].get({ plain: true }));
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
+// router.get('/', (req, res) => {
+//     Person.findAll({
+//         attributes: [
+//             'id',
+//             'last_name',
+//             'first_name'
+//         ]
+//     })
+//     .then(dbPersonData => {
+//         console.log(dbPersonData);
+//         const persons = dbPersonData.map(post=> post.get({ plain: true}))
+//         res.render('homepage', { 
+//             persons,
+//             loggedIn: req.session.loggedIn
+//          }) 
+        
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err)
+//     })
+// });
 
 router.get(`/login`, (req, res) => {
     if (req.session.loggedIn) {
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes')
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
 const profileRoutes= require('./profile-routes');
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 router.use('/profile', profileRoutes)
 
 router.use((req, res) => {
