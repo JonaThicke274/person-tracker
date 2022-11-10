dd4a176fc5d49cb266bc335a952a222badd5b043
tmunkhb <tmunkhb@gmail.com>
2022-11-10 18:33:29 -0500

    edit delete-profile button

--- public/javascript/delete-profile.js
+++ public/javascript/delete-profile.js
@@ -2,12 +2,12 @@ async function deleteFormHandler(event) {
     event.preventDefault();
   
     // will delete any person that has url with any user logged
-    // const id = window.location.toString().split('/')[
-    //     window.location.toString().split('/').length - 1
-    //   ];
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
     
     // will only delete person under logged user
-    const id = 3
+    // const id = 3
     const response = await fetch(`/api/person/${id}`, {
       method: 'DELETE'
     });
