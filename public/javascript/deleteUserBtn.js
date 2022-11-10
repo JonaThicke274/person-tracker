eb974ce7ca6e8b5cd64ebd31442afe4224299bc3
tmunkhb <tmunkhb@gmail.com>
2022-11-09 21:15:44 -0500

    working on deletebtn

---
+++ public/javascript/deleteUserBtn.js
@@ -0,0 +1,6 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+    console.log(true)
+}  
+  document.getElementById('deletePerson').addEventListener('click', deleteFormHandler);
+  
